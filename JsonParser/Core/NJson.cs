1d950e9c5ebeae34197fccbc9bcec8bd960afafe
NETools <enes.hergul215@gmail.com>
2023-03-17 20:16:13 +0100

    Add object

--- JsonParser/Core/NJson.cs
+++ JsonParser/Core/NJson.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using JsonParser.Structs;
+using JsonParser.Structs.Objects;
 using JsonParser.Tokenizer;
 
 namespace JsonParser.Core
@@ -32,6 +33,9 @@ namespace JsonParser.Core
             _rules.AddProcessor(ProcessDictionary,
                 (t) => t.Namespace == "System.Collections.Generic",
                 (t) => t.Name.Contains("Dictionary"));
+            _rules.AddProcessor(ProcessObject,
+                (t) => t.Namespace == "System",
+                (t) => t == typeof(object));
 
             _rules.AddMapper(
                 (name) => $"m_{name}",
@@ -84,6 +88,51 @@ namespace JsonParser.Core
             }
         }
 
+        private object ProcessObject(Type currentType, IEnumerator<JsonToken> tokenStream, Func<Type, NJsonInstanciatorResult> classInstanciator)
+        {
+            tokenStream.MoveNext();
+
+            switch (tokenStream.Current.TokenType)
+            {
+                case TokenType.String:
+                    return new JsonValue() { Value = tokenStream.Current.Value };
+                case TokenType.QuoteSign:
+                    var jsonObject = new JsonValue();
+                    tokenStream.MoveNext();
+                    jsonObject.Value = tokenStream.Current.Value;
+                    tokenStream.MoveNext();
+                    return jsonObject;
+                    break;
+                case TokenType.BracketOpen:
+                    var subJson = ReadBetweenBrackets(tokenStream);
+                    var jsonClassObject = new JsonClassObject();
+                    var subTokenStream = JsonTokenizer.NormalizeTokenStream(JsonTokenizer.TokenizeJson(subJson)).GetEnumerator();
+                    subTokenStream.MoveNext();
+
+                    while (subTokenStream.MoveNext())
+                    {
+                        if (subTokenStream.Current.TokenType == TokenType.Comma)
+                            subTokenStream.MoveNext();
+                        if (subTokenStream.Current.TokenType != TokenType.QuoteSign)
+                            break;
+                        subTokenStream.MoveNext();
+                        string variableName = subTokenStream.Current.Value;
+                        jsonClassObject[variableName] = null;
+                        subTokenStream.MoveNext();
+                        subTokenStream.MoveNext();
+
+                        jsonClassObject[variableName] = (IJsonObject)ProcessObject(null, subTokenStream, classInstanciator);
+                    }
+
+                    return jsonClassObject;
+                case TokenType.BraceOpen:
+
+                    break;
+            }
+
+            throw new NotImplementedException("Object types are not supported yet.");
+        }
+
         private object ProcessDictionary(Type currentType, IEnumerator<JsonToken> tokenStream, Func<Type, NJsonInstanciatorResult> classInstaciator)
         {
             var keyType = currentType.GenericTypeArguments[0];
@@ -96,10 +145,8 @@ namespace JsonParser.Core
             var genericDictionaryType = dictionaryType.MakeGenericType(keyType, valueType);
             var dictionary = (IDictionary)Activator.CreateInstance(genericDictionaryType);
 
-            while (tokenStream.MoveNext())
+            while (tokenStream.MoveNext() && tokenStream.Current.TokenType != TokenType.BracketClose)
             {
-                if (tokenStream.Current.TokenType == TokenType.BracketClose)
-                    break;
                 var parsedKey = keyProcessor.Invoke(keyType, tokenStream, classInstaciator);
                 tokenStream.MoveNext();
                 if (tokenStream.Current.TokenType != TokenType.Colon)
--- JsonParser/Program.cs
+++ JsonParser/Program.cs
@@ -1,39 +1,62 @@
 ﻿using JsonParser.Core;
-using JsonParser.Structs;
-using System.Reflection;
+using System.Text.Json;
 
 Console.WriteLine("BismIllah");
 
 NJson nJson = new NJson();
 
-A a = new A();
-a.Data = "rofl";
-a.C = new C() { AA = 1.91 };
-a.C.Tuple = new Tuple<string, string>("", "");
-a.Tuple = new Tuple<string, C>("aklerg", new C() { AA = 1919191, Tuple = new Tuple<string, string>("rofl", "lol") });
-a.Strings.Add("1");
-a.Strings.Add("2");
+//A a = new A();
+//a.Data = "rofl";
+//a.C = new C() { AA = 1.91 };
+//a.C.Tuple = new Tuple<string, string>("", "");
+//a.Tuple = new Tuple<string, C>("aklerg", new C() { AA = 1919191, Tuple = new Tuple<string, string>("rofl", "lol") });
+//a.Strings.Add("1");
+//a.Strings.Add("2");
 
-a.Cs.Add(new C() { AA = 2, Tuple = new Tuple<string, string>("a", "b") });
-a.Cs.Add(new C() { AA = 1, Tuple = new Tuple<string, string>("a", "ba") });
+//a.Cs.Add(new C() { AA = 2, Tuple = new Tuple<string, string>("a", "b") });
+//a.Cs.Add(new C() { AA = 1, Tuple = new Tuple<string, string>("a", "ba") });
 
-a.Test.Add(new List<string>(new string[] { "a, b, c, d, e, f" }));
+//a.Test.Add(new List<string>(new string[] { "a, b, c, d, e, f" }));
 
-a.Mappings.Add("a", new C() { AA = 5 });
-a.Mappings.Add("b", new C() { AA = 6 });
+//a.Mappings.Add("a", new Dictionary<string, C>() { { "rofl", new C() { AA = 5 } } });
+//a.Mappings.Add("b", new Dictionary<string, C>() { { "lol", new C() { AA = 15 } } });
 
-var json = nJson.SerializeInstance(a);
 
-A deserializedA = new A();
-nJson.DeserializeIntoInstance(json, deserializedA, (type) =>
-{
-    NJsonInstanciatorResult rslt = new();
-    rslt.Code = NJsonInstanciatorResultCode.Failed;
+//a.B = new B() { C = new C() { AA = 19119191919191919, Tuple = new Tuple<string, string>("rroror", "aojidwsefo") }, Data = "ihuwefuh" };
+//var json = nJson.SerializeInstance(a);
 
-    return rslt;
-});
+//A deserializedA = new A();
+//nJson.DeserializeIntoInstance(json, deserializedA, (type) =>
+//{
+//    NJsonInstanciatorResult rslt = new();
+//    rslt.Code = NJsonInstanciatorResultCode.Failed;
 
-Console.WriteLine();
+//    return rslt;
+//});
+
+T t = new T();
+t.Objects.Add("rofl");
+t.Objects.Add(1.5);
+t.Objects.Add(191);
+t.Objects.Add(new C() { AA = 215, Tuple = new Tuple<string, string>("a", "b") });
+var json = nJson.SerializeInstance(t);
+
+//var Tt = new T();
+//nJson.DeserializeIntoInstance(json, Tt, (type) =>
+//{
+//    NJsonInstanciatorResult rslt = new();
+//    rslt.Code = NJsonInstanciatorResultCode.Failed;
+
+//    return rslt;
+//});
+
+
+var tt = JsonSerializer.Deserialize<T>(json);
+var s = tt.Objects[3];
+
+
+
+Console.WriteLine(s.GetType());
 
 
 class C
@@ -53,11 +76,16 @@ class A
 
     public string Data { get; set; }
     public C C { get; set; }
+    public B B { get; set; }
     public Tuple<string, C> Tuple { get; set; }
     public List<string> Strings { get; set; } = new List<string>();
-    public Dictionary<string, C> Mappings { get; set; } = new();
+    public Dictionary<string, Dictionary<string, C>> Mappings { get; set; } = new();
     public List<C> Cs { get; set; } = new List<C>();
-
     public List<List<string>> Test { get; set; } = new List<List<string>>();
    
+}
+
+public class T
+{
+    public List<object> Objects { get; set; } = new List<object>();
 }
